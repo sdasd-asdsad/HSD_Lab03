8c852056550feab600d14faed411c0d0ef828c68
sdasd-asdsad <nanjin96@snu.ac.kr>
2021-03-17 11:21:45 +0900

    Copy from original project folder

---
+++ lab03_0319.srcs/sim_1/new/my_add.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/03/14 20:21:04
+// Design Name: 
+// Module Name: my_add
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_add(
+
+    );
+endmodule
---
+++ lab03_0319.srcs/sources_1/new/my_add.v
@@ -0,0 +1,26 @@
+module my_add #(
+        parameter BITWIDTH = 32
+    )
+   (
+        input [BITWIDTH-1:0] ain,
+        input [BITWIDTH-1:0] bin,
+        output [BITWIDTH-1:0] dout,
+        output overflow
+   );
+   
+   reg carry = 1'b0;
+   reg [BITWIDTH-1:0] temp_dout;
+   integer i;
+   
+   always @(*) begin
+        for (i=0;i<BITWIDTH;i=i+1)
+            begin
+            temp_dout[i] = (ain[i] ^ bin[i] ^ carry);
+            carry = (ain[i] & bin[i])|(ain[i] & carry)|(bin[i] & carry);
+        end
+   end
+     
+   assign overflow = carry;
+   assign dout = temp_dout;
+   
+endmodule
