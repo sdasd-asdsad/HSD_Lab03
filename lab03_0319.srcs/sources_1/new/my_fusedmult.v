70aa65920337d9a0c5594a5c2ab6e9a00b355119
sdasd-asdsad <nanjin96@snu.ac.kr>
2021-03-18 07:05:43 +0900

    Update my_fusedmult.v

--- lab03_0319.srcs/sources_1/new/my_fusedmult.v
+++ lab03_0319.srcs/sources_1/new/my_fusedmult.v
@@ -1,26 +1,31 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/03/14 20:49:32
-// Design Name: 
-// Module Name: my_fusedmult
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module my_fusedmult(
-
+module my_fusedmult #(
+        parameter BITWIDTH = 32
+    )
+   (
+        input [BITWIDTH-1:0] ain,
+        input [BITWIDTH-1:0] bin,
+        input en,
+        input clk,
+        output [2*BITWIDTH-1:0] dout
     );
+    
+    reg [2*BITWIDTH-1:0] acc;
+    wire [2*BITWIDTH-1:0] tempMult;
+    wire [2*BITWIDTH-1:0] tempAdd;
+    
+    
+    
+    always @(posedge clk) begin
+        if(en == 1) begin
+            acc = tempAdd;
+        end
+        else begin
+            acc = 0;
+        end
+    end
+    
+    my_mul #(.BITWIDTH(BITWIDTH)) mult(.ain(ain),.bin(bin),.dout(tempMult));
+    my_add #(.BITWIDTH(BITWIDTH)) add(.ain(acc),.bin(tempMult),.dout(tempAdd),.overflow());
+    assign dout = acc;
+    
 endmodule
